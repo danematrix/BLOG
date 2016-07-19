8af958ed732c8d6eb3a560a71a81a79ed7f8da82
Dane Matrix <matrixdane1@gmail.com>
2016-07-19 17:07:50 -0400

    validation

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -4,6 +4,7 @@ class PostsController < ApplicationController
 	end
 
 	def new
+		@post = Post.new
 		
 	end
 
@@ -11,7 +12,11 @@ class PostsController < ApplicationController
 		@post = Post.new(post_params)
 		@post.save
 
-		redirect_to @post
+		if @post.save
+			redirect_to @post
+		else
+			render 'new'
+		end
 	end
 
 	def show
